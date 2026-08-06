8b180b41b000c293be7907250f40223e1f1bf3a2
amariles-c <amariles.cosmo@escola.pr.gov.br>
2026-08-06 08:08:00 -0300

    .

---
+++ script.js
@@ -0,0 +1,35 @@
+function horarios(){
+    alert(
+`HORÁRIOS
+
+Segunda a Sexta
+06:00 às 22:00
+
+Sábado
+08:00 às 18:00
+
+Domingo
+08:00 às 12:00`
+    );
+}
+
+function valores(){
+    alert(
+`PLANOS
+
+Mensal: R$ 89,90
+
+Trimestral: R$ 239,90
+
+Anual: R$ 799,90`
+    );
+}
+
+function localizacao(){
+    alert(
+`LOCALIZAÇÃO
+
+Rua das Palmeiras, 250
+Centro - Sua Cidade`
+    );
+}
